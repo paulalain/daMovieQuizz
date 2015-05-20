045cbb0d6a79785fc66cf76e9b0949d2004b17cd
Paul-Alain Bugnard <paul.alain.bugnard@gmail.com>
2015-05-20 10:16:27 +0200

    Better testcases for game model

--- test/models/game.spec.js
+++ test/models/game.spec.js
@@ -9,38 +9,116 @@ describe('Game Model', function () {
 
 	describe('Start game', function () {
         it('state == 1', function () {
-          this.GameModel.startGame();
-          assert.equal(this.GameModel.get('state'), 1);
+          var model = this.GameModel;
+          model.startGame()
+            .then(function(g){
+              assert.equal(model.get('state'), 1);
+            }, function(error){
+              assert(false, "Error on start game");
+            });
+          
         });
 
         it('score == 0', function () {
-          this.GameModel.startGame();
-          assert.equal(this.GameModel.get('score'), 0);
+          var model = this.GameModel;
+          model.startGame()
+          .then(function(g){
+              assert.equal(model.get('score'), 0);
+            }, function(error){
+               assert(false, "Error on start game");
+            });
         });
 	});
 
 	describe('Stop game', function () {
         it('state == 2', function () {
-          this.GameModel.startGame();
-          this.GameModel.stopGame();
-          assert.equal(this.GameModel.get('state'), 2);
+          var model = this.GameModel;
+
+          model.startGame()
+          .then(function(g){
+            model.stopGame().
+              then(function(g){
+                 assert.equal(model.get('state'), 2);
+              }, function(error){
+                 assert(false, "Error on stop game");
+              });
+          }, function(error){
+            assert(false, "Error on start game");
+          });
         });
 	});
 
 	describe('Restart game', function () {
         it('state == 1', function () {
-          this.GameModel.startGame();
-          this.GameModel.stopGame();
-          this.GameModel.restartGame();
-          assert.equal(this.GameModel.get('state'), 1);
+          var model = this.GameModel;
+          this.GameModel.startGame()
+          .then(function(g){
+            model.stopGame()
+              .then(function(g){
+                model.restartGame()
+                  .then(function(g){
+                    assert.equal(model.get('state'), 1);
+                  }, function(error){
+                    assert(false, "Error on restart game");
+                  });
+              }, function(error){
+                assert(false, "Error on stop game");
+              });
+          }, function(error){
+            assert(false, "Error on start game");
+          });
+        });
+
+        it('score == 0', function () {
+          var model = this.GameModel;
+          model.startGame()
+            .then(function(g){
+              model.stopGame()
+                .then(function(g){
+                  model.restartGame()
+                    .then(function(g){
+                      assert.equal(model.get('score'), 0);
+                    }, function(error){
+                      assert(false, "Error on restart game");
+                    });
+                }, function(error){
+                  assert(false, "Error on stop game");
+                });
+            }, function(error){
+              assert(false, "Error on start game");
+            });
         });
 	});
     
     describe('Increment Score And Fetch New Question', function () {
         it('score > 0', function () {
-          this.GameModel.startGame();
-          this.GameModel.incrementScoreAndFetchNewQuestion();
-          assert.equal(this.GameModel.get('state'), 1);
+          var model = this.GameModel;
+          this.GameModel.startGame()
+            .then(function(g){
+              model.incrementScoreAndFetchNewQuestion()
+                .then(function(g){
+                  assert.equal(this.GameModel.get('state'), 1);
+                }, function(error){
+                  assert(false, "Error fetch new question");
+                })
+             }, function(error){
+                  assert(false, "Error on start game");
+            });
+      });
+
+        it('question != null', function () {
+          var model = this.GameModel;
+          this.GameModel.startGame()
+            .then(function(g){
+              model.incrementScoreAndFetchNewQuestion()
+                .then(function(g){
+                  assert.notEqual(this.GameModel.get('question'), null);
+                }, function(error){
+                  assert(false, "Error fetch new question");
+                })
+            }, function(error){
+              assert(false, "Error on start game");
+            });
         });
-	});
+	 });
 });
